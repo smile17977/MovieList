e8605a1bf8b17f47c7568acd9d1e51f984c965a0
smile17977 <65645958+smile17977@users.noreply.github.com>
2022-02-14 16:50:51 +0300

    Initial Commit

---
+++ MoviePremieresList/MoviePremieresListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePremieresListApp.swift
+//  MoviePremieresList
+//
+//  Created by Kirill Presnyakov on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviePremieresListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
